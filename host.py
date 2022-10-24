10829beba6e64a3b1958c91d57c47b7616cdbd60
Joan Nabirye <107198755+NabiryeJoan@users.noreply.github.com>
2022-10-24 10:13:15 +0300

    commite

---
+++ host.py
@@ -0,0 +1,14 @@
+name =input("what is your name? ")
+print("hello",name )
+age = input("what is your age ?")
+print("ooh ok", name)
+problem = input("what is the problem")
+print("we are here to help your", name)
+sick = help
+
+if sick:
+    print(" which disease",name )
+ 
+    
+    print("talk to us about it")
+   
---
+++ project.py
@@ -0,0 +1,14 @@
+from statistics import quantiles
+
+
+name = input("what is your name ?")
+print("Hello",name)
+item = input ("what items are  you taking ?")
+print("ok",name)
+unit_cost = float(input("what is the unit_cost?"))
+quantity = float(input("what is the quantity?"))
+total_cost = quantity * unit_cost
+print(total_cost)
+print("*"*20)
+print("Thank for shopping with us")
+print("we should expect next time ")
